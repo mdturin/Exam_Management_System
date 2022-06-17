0e6124ed4e04282a3f45614a522f14b8b240c72e
MD TURIN <matkhan01@yahoo.com>
2022-06-17 19:09:49 +0600

    media dir changed

--- app_test/models.py
+++ app_test/models.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from django.contrib.auth.models import User
 from django.db import models
 
--- main/settings.py
+++ main/settings.py
@@ -17,7 +17,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -123,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
