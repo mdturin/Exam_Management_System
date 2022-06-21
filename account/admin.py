d392ebf1b184c7741c751d467ac2a5b29cc3cdb9
MD TURIN <matkhan01@yahoo.com>
2022-06-21 18:12:06 +0600

    cleanup

--- account/admin.py
+++ account/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from account.models import *
 
 admin.site.register(Teacher)
-# admin.site.register(Faculty)
-# admin.site.register(Department)
-# admin.site.register(Course)
+admin.site.register(Dean)
+admin.site.register(Staff)
