9d2be54d2a18cc68d964f12c483c5b765a1e9ad5
MD TURIN <matkhan01@yahoo.com>
2022-06-23 01:36:35 +0600

    added otp

--- account/views.py
+++ account/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 from account.forms import *
+from account.mail_sender import send_code
 from account.models import *
 
 
 def register_page(request):
-    return render(request, 'register_page.html', {'title': 'Sign Up'})
+
+    context = {'title': 'Sign Up'}
+
+    if request.method == 'POST':
+        email = request.POST.get('email', '')
+
+        try:
+            user = User.objects.get(email=email)
+
+        except User.DoesNotExist:
+            print(email, 'dosent exist')
+            return render(request, 'register_page.html', context)
+
+        context['code'] = send_code(email)
+
+    return render(request, 'register_page.html', context)
