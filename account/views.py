141f24e8de853bcec4f6793cf7c705ddbd255da5
MD TURIN <matkhan01@yahoo.com>
2022-06-26 23:17:33 +0600

    login code added

--- account/views.py
+++ account/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
 from account.forms import *
 from account.mail_sender import send_code
 from account.models import *
-from django.views.generic.base import TemplateView
+
 
 class LoginCodeView(TemplateView):
     template_name = "login_code.html"
 
+
 class SetPasswordView(TemplateView):
     template_name = "set_password.html"
+
 
 def register_page(request):
 
@@ -33,5 +37,8 @@ def register_page(request):
             otp_user.save()
         except OTP.DoesNotExist:
             otp_user = OTP.objects.create(user=user, code=code)
+
+        context = {'title': 'Verify User'}
+        return render(request, 'login_code.html', context)
 
     return render(request, 'register_page.html', context)
