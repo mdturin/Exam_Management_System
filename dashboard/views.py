6fa3d1dfd6b6ba63c3fe7317e44896506717c3a2
MD TURIN <matkhan01@yahoo.com>
2022-06-28 02:07:30 +0600

    clean up

--- dashboard/views.py
+++ dashboard/views.py
@@ -78,7 +78,4 @@ def dashboard(request):
         context['routines'] = get_routines(faculty)
         context['teachers'] = get_teachers(faculty_name)
 
-        print(faculty_name)
-        print(context['routines'])
-
     return render(request, 'dashboard.html', context)
