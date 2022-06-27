ef59960d4d42165d09d9536bebb697faae29258b
MD TURIN <matkhan01@yahoo.com>
2022-06-27 20:59:56 +0600

    not done

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,39 @@
 from django.shortcuts import render
+from account.models import *
+
+
+def is_dean(user):
+    try:
+        teacher = Teacher.objects.get(user=user)
+    except Teacher.DoesNotExist:
+        return None
+
+    deans = Dean.objects.filter(teacher=teacher).order_by('-joined')[:1]
+    if len(deans) != 1:
+        return None
+
+    return deans[0]
+
+
+def is_staff(user):
+    try:
+        staff = Staff.objects.get(user=user)
+    except Staff.DoesNotExist:
+        return None
+    return staff
+
+
+def has_permission(user):
+    return is_staff(user) != None or is_dean(user) != None
+
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    context = {
+        'is_staff': has_permission(request.user)
+    }
+
+    if context['is_staff']:
+        context['staffs'] = Staff.objects.all()
+        context['routines'] = Routine.objects.all()
+
+    return render(request, 'dashboard.html', context)
