b591a84b9de39313c709d5b800db9834417bb4e0
Md Turin <matkhan01@yahoo.com>
2022-08-12 02:14:12 +0600

    event date fixed

--- dashboard/models.py
+++ dashboard/models.py
@@ -56,13 +56,11 @@ class Event(models.Model):
 
     name = models.CharField(max_length=255)
 
-    start_date = models.DateField()
+    start_date = models.DateTimeField()
 
-    end_date = models.DateField()
+    end_date = models.DateTimeField()
 
     notes = models.TextField(blank=True)
 
     def __str__(self) -> str:
         return self.name
-
-    
