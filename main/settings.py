1feee3c9da55f4b9f70846e9ce80eea05457b646
mdturin <matkhan01@yahoo.com>
2023-01-22 00:11:12 +0600

    done

--- main/settings.py
+++ main/settings.py
@@ -151,7 +151,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'matkhan01@gmail.com'
+EMAIL_HOST_USER = 'Your-email'
 EMAIL_HOST_PASSWORD = 'Your-Password'
 RECIPIENT_ADDRESS = 'example@mail.com'
 
